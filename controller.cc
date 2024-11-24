82fa320002ee85d390a0639d76caece33bbc2901
Mari Montoya <kulafumon@gmail.com>
2024-11-23 23:53:55 -0500

    Added input interpretation and some methods related to game state to Controller. Maybe add the repetition check in interpretInput too?

--- controller.cc
+++ controller.cc
@@ -1,5 +1,24 @@
 #include "controller.h"
+#include <sstream>
+#include "board.h"
 
+// Big 5
+Controller::Controller(int seed, int startingLevel, std::string scriptFile1, std::string scriptFile2) :
+  seed{seed}, startingLevel{startingLevel}, scriptFile1{scriptFile1}, scriptFile2{scriptFile2} {
+  for(int i = 0; i < numberOfPlayers; i++){
+    // TODO MARI: Create boards
+  }
+}
+
+// Accessors
+const std::vector<Board*>& Controller::getBoards() const {return boards;}
+
+Board* Controller::getBoard() const {return boards.at(currentPlayer % boards.size());}
+
+// Mutators
+void Controller::nextPlayer(){currentPlayer++;}
+
+// Display-related
 void Controller::notifyObservers() const {
   for(View* view : observers){
      view->notify();
@@ -18,4 +37,44 @@ void Controller::detachView(View* viewer){
     if(*it == viewer) it = observers.erase(it);
     else it++;
   }
+}
+
+// For the game TODO: MARI
+void Controller::sequence(std::string){
+
+}
+
+void Controller::noRandom(std::string){
+
+}
+
+void Controller::random(){
+
+}
+
+void Controller::resetGame(){
+  for(Board* board : boards){
+    board->clearBoard();
+    board->forceLevel(startingLevel);
+    board->setScore(0);
+  }
+  currentPlayer = 0;
+}
+
+// I/O-related
+Controller::Command Controller::interpretInput(const std::string input) const {
+  // Check if this is a known command
+  for(auto stringCommand : commands){
+    if(stringCommand.first == input) return stringCommand.second;
+  }
+
+  // Check if enough of a string was given to distinguish it from other commands
+  Command match = Command::INVALID;
+  for(auto stringCommand : commands){
+    if(!(stringCommand.first.startsWith(input))) continue; // input isn't a prefix of command, so can't be that
+    else if(match == Command::INVALID) match = stringCommand.second; // found it as a prefix for a known command
+    else return Command::INVALID; // found it as a prefix for another command, so input is ambiguous
+  }
+
+  return match;
 }
--- controller.h
+++ controller.h
@@ -1,21 +1,70 @@
 #ifndef CONTROLLER_H
 #define CONTROLLER_H
 #include <vector>
+#include <string>
+#include <map>
+#include "board.h"
 
 class View; // forward declaration
 
 class Controller{
-  // std::vector<Player*> players; TODO
+  const int seed;
+  const int startingLevel;
+  const std::string scriptFile1;
+  const std::string scriptFile2;
+  const int numberOfPlayers = 2;
+  std::vector<Board*> boards;
   std::vector<View*> observers;
+  int currentPlayer = 0;
+
+  std::map<std::string, Command> commands{
+    {"left", Command::LEFT}, {"right", Command::RIGHT}, {"down", Command::DOWN}, {"clockwise", Command::CLOCKWISE},
+    {"counter_clockwise", Command::COUNTER_CLOCKWISE}, {"drop", Command::DROP}, {"random", Command::RANDOM},
+    {"sequence", Command::SEQUENCE}, {"I", Command::I}, {"J", Command::J}, {"L", Command::L}, {"O", Command::O},
+    {"S", Command::S}, {"Z", Command::Z}, {"T", Command::T}, {"restart", Command::RESTART},
+    {"blind", Command::BLIND}, {"heavy", Command::HEAVY}, {"force", Command::FORCE}
+  };
 
   public:
+    // Big 5
+    Controller(int, int, std::string, std::string);
+
+    // Accessors
+    const std::vector<Board*>& getBoards() const;
+
+    const Board* getBoard() const;
+
+    // Mutators
+    void nextPlayer();
+
+    // Display-related
     void notifyObservers() const;
 
     void attachView(View*);
 
     void detachView(View*);
 
-    const std::vector<Player*>& getPlayers() const;
+    // For the game
+    void sequence(std::string);
+
+    void noRandom(std::string);
+
+    void random();
+
+    void resetGame();
+
+    void nextPlayer();
+
+    // I/O-related
+    enum class Command{
+      LEFT, RIGHT, DOWN, CLOCKWISE, COUNTER_CLOCKWISE, DROP, // block movement
+      LEVEL_UP, LEVEL_DOWN, NO_RANDOM, RANDOM, SEQUENCE, // specify block generation
+      I, J, L, O, S, Z, T, // specify next block
+      RESTART, INVALID, // other
+      BLIND, HEAVY, FORCE // special actions
+    };
+
+    Controller::Command interpretInput(const std::string) const;
 };
 
 #endif //CONTROLLER_H
