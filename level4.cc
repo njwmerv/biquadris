d14c66498764d0d221415454958b3d1f608a8cba
Mari Montoya <kulafumon@gmail.com>
2024-11-25 22:43:06 -0500

    Fixed level4 and made TODO for place1Block()

--- level4.cc
+++ level4.cc
@@ -1,21 +1,15 @@
 #include "level4.h"
-#include "iblock.h"
-#include "jblock.h"
-#include "lblock.h"
-#include "oblock.h"
-#include "sblock.h"
-#include "zblock.h"
-#include "tblock.h"
-
+#include "level3.h"
 using namespace std;
 
 
-Block *Level4::generateBlock() {
+Block* Level4::generateBlock() {
     blockCount++;
     if (blockCount % 5 == 0) {
-        place1Block();
+        // place1Block(); TODO Chloe
     }
-    return Level3::generateBlock();
+    Level3 temp;
+    return temp.generateBlock();
 
 }
 
