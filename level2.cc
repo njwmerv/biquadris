133c3790aca4edfcba143b29bc4a52f7af6bc4b6
Mari Montoya <kulafumon@gmail.com>
2024-11-25 22:36:35 -0500

    Fixed typo level2 so now the blocks are actually level 2.

--- level2.cc
+++ level2.cc
@@ -14,12 +14,12 @@ Block *Level2::generateBlock(){
     const string blockTypes[] = {"I", "J", "I", "L", "O", "S", "Z", "T"};
     const string blockGenerated = blockTypes[rand() % 7];
 
-    if(blockGenerated == "I") return new IBlock(1);
-    else if(blockGenerated == "J") return new JBlock(1);
-    else if(blockGenerated == "L") return new LBlock(1);
-    else if(blockGenerated == "O") return new OBlock(1);
-    else if(blockGenerated == "S") return new SBlock(1);
-    else if(blockGenerated == "Z") return new ZBlock(1);
-    else return new TBlock(1);
+    if(blockGenerated == "I") return new IBlock(2);
+    else if(blockGenerated == "J") return new JBlock(2);
+    else if(blockGenerated == "L") return new LBlock(2);
+    else if(blockGenerated == "O") return new OBlock(2);
+    else if(blockGenerated == "S") return new SBlock(2);
+    else if(blockGenerated == "Z") return new ZBlock(2);
+    else return new TBlock(2);
 }
 
