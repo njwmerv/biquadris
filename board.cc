5d8719370241a52de8cda1e6db397f2bf5566721
Mari Montoya <kulafumon@gmail.com>
2024-11-25 21:39:19 -0500

    Changed initialization of board.

--- board.cc
+++ board.cc
@@ -10,10 +10,11 @@
 using namespace std;
 
 // Big 5
-Board::Board(int startingLevel, std::string level0File) :
-  score{0}, level0File{level0File}, currentLevel{startingLevel} {
+Board::Board(int startingLevel, string level0File) : score{0}, level0File{level0File}, currentLevel{startingLevel} {
   for(int i = 0; i < 15; i++){
-    board.emplace_back(vector<shared_ptr<Block>> {nullptr});
+    vector<shared_ptr<Block>> row;
+    for(int j = 0; j < 11; j++) row.emplace_back(nullptr);
+    board.emplace_back(row);
   }
   if(startingLevel == 0) level = new Level0{level0File};
   else if(startingLevel == 1) level = new Level1;
