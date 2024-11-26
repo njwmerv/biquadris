7db302cc5cc8d93c0333254816db85b352f19685
Harry Cheng <harryc3015@gmail.com>
2024-11-26 17:41:16 -0500

    testing

--- board.cc
+++ board.cc
@@ -160,11 +160,13 @@ void Board :: right() {
   for(pair<int, int> cell : current->getRotation(curNumRot)) {
     board[cell.first + curX][cell.second + curY] = nullptr;
   }
+  /*
   current->setX(curX+1);
   curX = curX + 1;
   for(pair<int, int> cell : current->getRotation(curNumRot)) {
     board[cell.first + curX][cell.second + curY] = current;
   }
+  */
   if(willDrop) drop();
 }
 
