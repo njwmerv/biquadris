b70a0301b85d9e82b7b5623dced6cdc41d99b400
Mari Montoya <kulafumon@gmail.com>
2024-11-25 19:24:46 -0500

    Fixed MIL for SBlock.

--- sblock.cc
+++ sblock.cc
@@ -3,11 +3,12 @@
 
 using namespace std;
 
-SBlock::SBlock(int l) : level{l} {
+SBlock::SBlock(int l){
     rotations[0] = {{0, 1}, {1, 1}, {1, 2}, {2, 2}};
     rotations[1] = {{1, 2}, {1, 1}, {2, 1}, {2, 0}};
     rotations[2] = rotations[0];
     rotations[3] = rotations[1];
+    level = l;
 }
 
 char SBlock::getType() const {
