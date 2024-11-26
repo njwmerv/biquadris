9b62cf288da6dcc7f92cbda430c99a55828e7785
Harry Cheng <harryc3015@gmail.com>
2024-11-25 20:14:02 -0500

    added setters for x and y in block

--- block.cc
+++ block.cc
@@ -5,3 +5,7 @@ using namespace std;
 void Block::clockwise(){numRotations = (numRotations + 1) % maxRotations;}
 
 void Block::counterclockwise(){numRotations = (numRotations - 1) % maxRotations;}
+
+void Block::setX(int newX) {x = newX;}
+
+void Block::setY(int newY) {y = newY;}
