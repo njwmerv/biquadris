a66c1d5c8a7bfc3d6007ab3e01d8c0a950939bf8
Mari Montoya <kulafumon@gmail.com>
2024-11-26 18:42:17 -0500

    Fixed rotation for TBlock and SBlock.

--- sblock.cc
+++ sblock.cc
@@ -4,8 +4,8 @@
 using namespace std;
 
 SBlock::SBlock(int l){
-    rotations[0] = {{0, 1}, {1, 1}, {1, 2}, {2, 2}};
-    rotations[1] = {{1, 2}, {1, 1}, {2, 1}, {2, 0}};
+    rotations[0] = {{0, 0}, {1, 0}, {1, 1}, {2, 1}};
+    rotations[1] = {{0, 2}, {0, 1}, {1, 1}, {1, 0}};
     rotations[2] = rotations[0];
     rotations[3] = rotations[1];
     level = l;
--- tblock.cc
+++ tblock.cc
@@ -4,10 +4,10 @@
 using namespace std;
 
 TBlock::TBlock(int l){
-    rotations[0] = {{0, 1}, {1, 1}, {1, 2}, {2, 1}};
-    rotations[1] = {{1, 2}, {1, 1}, {1, 0}, {2, 1}};
-    rotations[2] = {{0, 1}, {1, 0}, {1, 1}, {2, 1}};
-    rotations[3] = {{0, 1}, {1, 0}, {1, 1}, {1, 2}};
+    rotations[0] = {{0, 0}, {1, 0}, {2, 0}, {1, 1}};
+    rotations[1] = {{0, 2}, {0, 1}, {0, 0}, {1, 1}};
+    rotations[2] = {{0, 1}, {1, 1}, {1, 2}, {1, 0}};
+    rotations[3] = {{0, 1}, {1, 1}, {1, 0}, {1, 2}};
 }
 
 char TBlock::getType() const {
