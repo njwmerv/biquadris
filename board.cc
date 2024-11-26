aa74fc45e07c3117339e78b3d7a253b260922fdb
Mari Montoya <kulafumon@gmail.com>
2024-11-25 21:22:25 -0500

    small changes in board.

--- board.cc
+++ board.cc
@@ -6,6 +6,7 @@
 #include "level2.h"
 #include "level3.h"
 #include "level4.h"
+using namespace std;
 
 // Big 5
 Board::Board(int startingLevel, std::string level0File) :
@@ -46,7 +47,7 @@ void Board::leveldown(){
   if(currentLevel == 0) return;
   delete level;
   currentLevel--;
-  if(currentLevel == 0) level = new Level0{level0File};
+  if(currentLevel == 0) level = new Level0(level0File);
   else if(currentLevel == 1) level = new Level1;
   else if(currentLevel == 2) level = new Level2;
   else if(currentLevel == 3) level = new Level3;
