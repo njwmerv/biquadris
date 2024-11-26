e974ebb7f93ec9b3e1345b60b95ed765565bdb63
Mari Montoya <kulafumon@gmail.com>
2024-11-26 18:12:06 -0500

    Fixed starting position for OBlock.

--- oblock.cc
+++ oblock.cc
@@ -4,7 +4,7 @@
 using namespace std;
 
 OBlock::OBlock(int l){
-    rotations[0] = {{1, 1}, {1, 2}, {2, 1}, {2, 2}};
+    rotations[0] = {{0, 2}, {0, 1}, {1, 2}, {1, 1}};
     rotations[1] = rotations[0];
     rotations[2] = rotations[0];
     rotations[3] = rotations[0];
