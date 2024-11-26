db6819e3ee38a3358e33cbd623a26657dbbad4ee
chloe778 <chloe47562@gmail.com>
2024-11-25 19:23:01 -0500

    lblock

---
+++ lblock.h
@@ -0,0 +1,28 @@
+#ifndef LBLOCK_H
+#define LBLOCK_H
+
+#include "block.h"
+
+
+using namespace std;
+
+class LBlock : public Block {
+    public: 
+        // constructor
+        LBlock(int l);
+
+        // destructor
+        ~LBlock() override;
+
+        // getter
+        char getType() const override;
+
+        // rotate the block clockwise
+        void clockwise () override;
+
+        // rotate the block counterclockwise
+        void counterclockwise () override;
+
+};
+
+#endif
