688d2376df63c0e18bacbd671678aff0ed2adae2
chloe778 <chloe47562@gmail.com>
2024-11-26 17:23:31 -0500

    fixed l block

--- lblock.cc
+++ lblock.cc
@@ -4,7 +4,7 @@
 using namespace std;
 
 LBlock :: LBlock(int l) {
-    rotations[0] = {{0, 1}, {1, 1}, {2, 0}, {2, 1}};
+    rotations[0] = {{0, 1}, {1, 1}, {2, 1}, {2, 2}};
     rotations[1] = {{1, 2}, {1, 1}, {1, 0}, {2, 0}};
     rotations[2] = {{0, 0}, {0, 1}, {1, 1}, {2, 1}};
     rotations[3] = {{0, 2}, {1, 2}, {1, 1}, {2, 0}};
