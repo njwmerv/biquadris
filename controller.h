fa10d79681e44b1900f09ded198d967d662bc2a6
m2montoy <m2montoy@uwaterloo.ca>
2024-11-27 17:53:27 -0500

    Put Commands enum at top of Controller class

--- controller.h
+++ controller.h
@@ -13,16 +13,6 @@
 using namespace std;
 
 class Controller{
-  const int seed;
-  const int startingLevel;
-  const string scriptFile1;
-  const string scriptFile2;
-  const int numberOfPlayers = 2;
-  int currentPlayer = 0;
-  vector<Board*> boards;
-  vector<View*> observers;
-  vector<queue<Command>> commandsToExecute;
-
   enum class Command{
     LEFT, RIGHT, DOWN, CLOCKWISE, COUNTER_CLOCKWISE, DROP, // block movement
     LEVEL_UP, LEVEL_DOWN, NO_RANDOM, RANDOM, SEQUENCE, // specify block generation
@@ -30,6 +20,16 @@ class Controller{
     RESTART, INVALID, // other
     BLIND, HEAVY, FORCE // special actions
   };
+
+	const int seed;
+	const int startingLevel;
+	const string scriptFile1;
+	const string scriptFile2;
+	const int numberOfPlayers = 2;
+	int currentPlayer = 0;
+	vector<Board*> boards;
+	vector<View*> observers;
+	vector<queue<Command>> commandsToExecute;
 
   map<string, Command> commands{
     {"left", Command::LEFT}, {"right", Command::RIGHT}, {"down", Command::DOWN}, {"clockwise", Command::CLOCKWISE},
