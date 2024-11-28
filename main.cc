02a83d377215c5db8a72a0249c1233db20af26a7
Mari Montoya <kulafumon@gmail.com>
2024-11-28 02:25:31 -0500

    Fixed seeding for randomness.

--- main.cc
+++ main.cc
@@ -7,51 +7,57 @@
 #include "text.h"
 #include "graphic.h"
 #include "block.h"
+using namespace std;
 
 int main(int argc, char* argv[]){
   // Game setup
   // Read CLI args
-  std::string arg;
+  string arg;
   bool textOnly = false;
-  int seed = std::rand();
   int startingLevel = 0;
-  std::string scriptFile1 = "sequence1.txt";
-  std::string scriptFile2 = "sequence2.txt";
+  string scriptFile1 = "sequence1.txt";
+  string scriptFile2 = "sequence2.txt";
   for(int i = 1; i < argc; i++){
-    std::istringstream iss{argv[i]};
+    istringstream iss{argv[i]};
     iss >> arg;
 
     if(arg == "-text") textOnly = true;
     else if(arg == "-seed" && i + 1 < argc){
-      std::string str = argv[i + 1];
-      std::istringstream seedInput{str};
-      seedInput >> seed;
+      string str = argv[i + 1];
+      if(str == "random"){
+        srand(time(nullptr));
+        i++;
+        continue;
+      }
+      int seed;
+      istringstream seedInput{str};
+      if(seedInput >> seed) srand(seed);
       i++;
     }
     else if(arg == "-startlevel" && i + 1 < argc){
-      std::string str = argv[i + 1];
-      std::istringstream startLevelInput{str};
+      string str = argv[i + 1];
+      istringstream startLevelInput{str};
       startLevelInput >> startingLevel;
       i++;
     }
     else if(arg == "-scriptfile1" && i + 1 < argc){
-      std::string str = argv[i + 1];
-      std::istringstream scriptFileInput{str};
+      string str = argv[i + 1];
+      istringstream scriptFileInput{str};
       scriptFileInput >> scriptFile1;
       i++;
     }
     else if(arg == "-scriptfile2" && i + 1 < argc){
-      std::string str = argv[i + 1];
-      std::istringstream scriptFileInput{str};
+      string str = argv[i + 1];
+      istringstream scriptFileInput{str};
       scriptFileInput >> scriptFile2;
       i++;
     }
-    else{std::cerr << arg << " is not a valid command line argument" << std::endl;}
+    else{cerr << arg << " is not a valid command line argument" << endl;}
   }
-  Controller controller{seed, startingLevel, scriptFile1, scriptFile2};
+  Controller controller{startingLevel, scriptFile1, scriptFile2};
 
   // Create displays
-  std::vector<View*> displays;
+  vector<View*> displays;
   displays.emplace_back(new Text(&controller));
   if(!textOnly) displays.emplace_back(new Graphic(&controller));
 
