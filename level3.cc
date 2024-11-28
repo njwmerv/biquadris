5912f1442dfad69607818230910255179e6adda3
Mari Montoya <kulafumon@gmail.com>
2024-11-28 00:07:23 -0500

    Added no random option for Level3.

--- level3.cc
+++ level3.cc
@@ -7,18 +7,37 @@
 #include "zblock.h"
 #include "tblock.h"
 #include <cstdlib>
+#include <fstream>
 
-Block *Level3::generateBlock() {
-    const string blockTypes[] = {"I", "J", "L", "O", "S", "S", "Z", "Z", "T"};
-    const string blockGenerated = blockTypes[rand() % 9];
-    Block* block = nullptr;
-    if(blockGenerated == "I") block = new IBlock(3);
-    else if(blockGenerated == "J") block = new JBlock(3);
-    else if(blockGenerated == "L") block = new LBlock(3);
-    else if(blockGenerated == "O") block = new OBlock(3);
-    else if(blockGenerated == "S") block = new SBlock(3);
-    else if(blockGenerated == "Z") block = new ZBlock(3);
-    else block = new TBlock(3);
-    block->changeWeight(1);
-    return block;
+Level3::Level3() : filename{""} {}
+
+Level3::Level3(const string& filename) : file{filename}, filename{filename} {}
+
+Block* Level3::generateBlock(){
+  Block* block = nullptr;
+  const string blockTypes[] = {"I", "J", "L", "O", "S", "S", "Z", "Z", "T"};
+  string blockGenerated;
+  if(isRandom){
+    blockGenerated = blockTypes[rand() % 9];
+  }
+  else{
+    if(!(file >> blockGenerated)){
+      file.close();
+      file.open(filename);
+    }
+    if(!(file >> blockGenerated)){
+      isRandom = true; // file given is just empty
+    }
+  }
+  if(blockGenerated == "I") block = new IBlock(3);
+  else if(blockGenerated == "J") block = new JBlock(3);
+  else if(blockGenerated == "L") block = new LBlock(3);
+  else if(blockGenerated == "O") block = new OBlock(3);
+  else if(blockGenerated == "S") block = new SBlock(3);
+  else if(blockGenerated == "Z") block = new ZBlock(3);
+  else block = new TBlock(3);
+  block->changeWeight(1);
+  return block;
 }
+
+void Level3::setRandomness(bool nowRandom) {isRandom = nowRandom;}
--- level3.h
+++ level3.h
@@ -1,15 +1,21 @@
 #ifndef LEVEL3_H
 #define LEVEL3_H
-
 #include "level.h"
+#include <fstream>
 #include <string>
 
 class Level3 : public Level {
-    public:
-        Block *generateBlock() override;
+  ifstream file;
+  string filename;
+  bool isRandom = false;
+
+  public:
+    Level3();
+    Level3(const string& file);
+
+    Block *generateBlock() override;
+
+    void setRandomness(bool);
 };
-
-
-
 
 #endif
